aef0220006ebeae3805777e4804f7496d27f3a5f
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-28 23:37:46 -0400

    Added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,10 +1,17 @@
-from django.contrib.auth.models import BaseUserManager
+from __future__ import annotations
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractUser,
+    PermissionsMixin,
+)
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.db import models
 
 # Create your models here.
 
 
 class UserManager(BaseUserManager):
-    def create(self, first_name, last_name, username, email, password):
+    def create(self, first_name, last_name, username, email, password) -> User:
         user = self.model(
             first_name=first_name,
             last_name=last_name,
@@ -18,7 +25,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(
         self, first_name, last_name, username, email, password
-    ):
+    ) -> User:
         user = self.create(first_name, last_name, username, email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -26,3 +33,23 @@ class UserManager(BaseUserManager):
 
     def normalize_email(self, email: str):
         return email.lower()
+
+
+class User(AbstractUser, PermissionsMixin):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    first_surname = models.CharField(max_length=50)
+    second_surname = models.CharField(max_length=50)
+    username = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[UnicodeUsernameValidator],
+    )
+    email = models.EmailField(max_length=255, unique=True)
+
+    objects = UserManager()
+
+    REQUIRED_FIELDS = ["first_name", "last_name", "username", "email"]
+
+    def __str__(self) -> str:
+        return self.username
