596f6ead0b591ac1ed62863ad11fe89724cd5f95
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-28 23:47:30 -0400

    Added subject model

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -53,3 +54,14 @@ class User(AbstractUser, PermissionsMixin):
 
     def __str__(self) -> str:
         return self.username
+
+
+class Subject(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, editable=False)
+    code = models.CharField(max_length=7, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    credits = models.IntegerField(default=0, validators=[MinValueValidator("0")])
+    is_lab = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.code} - {self.name}"
