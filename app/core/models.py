d21dcd7d097cd39ed7b87f4a3024b4a7e00b7762
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-28 23:30:31 -0400

    Added custom user manager

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,28 @@
-from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 
 
+class UserManager(BaseUserManager):
+    def create(self, first_name, last_name, username, email, password):
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self, first_name, last_name, username, email, password
+    ):
+        user = self.create(first_name, last_name, username, email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+    def normalize_email(self, email: str):
+        return email.lower()
