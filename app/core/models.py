7db5b07cd828f4f2792566f183637834fa9ae7cb
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-29 00:00:45 -0400

    Added selection model

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from django.conf import settings
 from django.contrib.auth.models import (
     BaseUserManager,
     AbstractUser,
@@ -61,7 +62,9 @@ class Subject(models.Model):
     id = models.AutoField(primary_key=True, unique=True, editable=False)
     code = models.CharField(max_length=7, unique=True)
     name = models.CharField(max_length=255, unique=True)
-    credits = models.IntegerField(default=0, validators=[MinValueValidator("0")])
+    credits = models.IntegerField(
+        default=0, validators=[MinValueValidator("0")]
+    )
     is_lab = models.BooleanField(default=False)
 
     def __str__(self) -> str:
@@ -71,6 +74,19 @@ class Subject(models.Model):
 class Weekday(models.Model):
     id = models.AutoField(primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Selection(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, editable=False)
+    name = models.CharField(max_length=100)
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.name
