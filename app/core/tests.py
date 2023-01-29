2fdd20c5d806c898303985a0005ceb4f312ce36b
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-01-28 23:09:28 -0400

    Added payload as global constant

--- app/core/tests.py
+++ app/core/tests.py
@@ -3,6 +3,14 @@ from django.test import TestCase
 
 # Create your tests here.
 
+PAYLOAD = {
+    "first_name": "Test",
+    "last_name": "User",
+    "username": "testuser",
+    "email": "testuser@example.com",
+    "password": "testpass123",
+}
+
 
 def create_user(**params):
     user = get_user_model().objects.create(**params)
@@ -11,78 +19,43 @@ def create_user(**params):
 
 class UserModelTests(TestCase):
     def test_create_user_success(self):
-        payload = {
-            "first_name": "Test",
-            "last_name": "User",
-            "username": "testuser",
-            "email": "testuser@example.com",
-            "password": "testpass123",
-        }
-        user = create_user(**payload)
+        user = create_user(**PAYLOAD)
 
-        self.assertEqual(user.first_name, payload["first_name"])
-        self.assertEqual(user.last_name, payload["last_name"])
-        self.assertEqual(user.username, payload["username"])
-        self.assertEqual(user.email, payload["email"])
-        self.assertTrue(user.check_password(payload["password"]))
+        self.assertEqual(user.first_name, PAYLOAD["first_name"])
+        self.assertEqual(user.last_name, PAYLOAD["last_name"])
+        self.assertEqual(user.username, PAYLOAD["username"])
+        self.assertEqual(user.email, PAYLOAD["email"])
+        self.assertTrue(user.check_password(PAYLOAD["password"]))
 
     def test_partial_update_user(self):
-        payload = {
-            "first_name": "Test",
-            "last_name": "User",
-            "username": "testuser",
-            "email": "testuser@example.com",
-            "password": "testpass123",
-        }
         new_username = "newtestuser"
-        user = create_user(**payload)
+        user = create_user(**PAYLOAD)
         get_user_model().objects.update(username=new_username)
         user.refresh_from_db()
 
-        self.assertEqual(user.first_name, payload["first_name"])
-        self.assertEqual(user.last_name, payload["last_name"])
+        self.assertEqual(user.first_name, PAYLOAD["first_name"])
+        self.assertEqual(user.last_name, PAYLOAD["last_name"])
         self.assertEqual(user.username, new_username)
-        self.assertEqual(user.email, payload["email"])
-        self.assertTrue(user.check_password(payload["password"]))
+        self.assertEqual(user.email, PAYLOAD["email"])
+        self.assertTrue(user.check_password(PAYLOAD["password"]))
 
     def test_delete_user(self):
-        payload = {
-            "first_name": "Test",
-            "last_name": "User",
-            "username": "testuser",
-            "email": "testuser@example.com",
-            "password": "testpass123",
-        }
-        user = create_user(**payload)
+        user = create_user(**PAYLOAD)
         user.delete()
-        user = get_user_model().objects.filter(username=payload["username"])
+        user = get_user_model().objects.filter(username=PAYLOAD["username"])
 
         self.assertFalse(user)
 
     def test_get_user(self):
-        payload = {
-            "first_name": "Test",
-            "last_name": "User",
-            "username": "testuser",
-            "email": "testuser@example.com",
-            "password": "testpass123",
-        }
-        create_user(**payload)
-        user = get_user_model().objects.get(username=payload["username"])
+        create_user(**PAYLOAD)
+        user = get_user_model().objects.get(username=PAYLOAD["username"])
 
         self.assertTrue(user)
 
     def test_get_all_users(self):
-        payload = {
-            "first_name": "Test",
-            "last_name": "User",
-            "username": "testuser",
-            "email": "testuser@example.com",
-            "password": "testpass123",
-        }
-        create_user(**payload)
+        create_user(**PAYLOAD)
 
-        payload.update(
+        PAYLOAD.update(
             {
                 "first_name": "User",
                 "last_name": "Test",
@@ -91,7 +64,7 @@ class UserModelTests(TestCase):
                 "password": "passtest123",
             }
         )
-        create_user(**payload)
+        create_user(**PAYLOAD)
 
         users = get_user_model().objects.all()
 
