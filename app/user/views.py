7e69012733becd719d6b9755485c506cc1cbb05d
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-02-02 18:43:32 -0400

    Added pair and refresh token views

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from user.serializers import PairTokenSerializer, RefreshTokenSerializer
 
 # Create your views here.
+
+
+class PairTokenView(TokenObtainPairView):
+    serializer_class = PairTokenSerializer
+
+
+class RefreshTokenView(TokenRefreshView):
+    serializer_class = RefreshTokenSerializer
