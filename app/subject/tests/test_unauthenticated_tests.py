09237c0d87fdb705e1e82efef6e02f5ab10ff3a7
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2023-02-03 18:29:56 -0400

    Add first test

---
+++ app/subject/tests/test_unauthenticated_tests.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from core.models import Subject
+from rest_framework.reverse import reverse
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient, force_authenticate
+from rest_framework import status
+
+SUBJECT_URL = reverse("subject:subject-list")
+
+
+# Create your tests here.
+class SubjectUnauthenticatedTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_create_subject_test_success(self) -> None:
+        res = self.client.get(SUBJECT_URL)
+        print(res.status_code)
+        self.assertEqual(1, 1)
