204d4536b7c72980316cbec81edd2005ec78d629
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-02-03 18:15:51 -0400

    Added user list view

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,8 @@
-from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -15,3 +18,59 @@ class PairTokenView(TokenObtainPairView):
 
 class RefreshTokenView(TokenRefreshView):
     serializer_class = serializers.RefreshTokenSerializer
+
+
+class UserListView(APIView):
+    permission_classes = [IsAdminUser]
+    serializer = serializers.UserSerializer
+
+    def get(self, request, format=None):
+        try:
+            users = get_user_model().objects.all()
+            serializer = self.serializer(users, many=True)
+
+            response = {
+                "status": "success",
+                "data": {
+                    "count": users.count(),
+                    "users": serializer.data,
+                },
+            }
+            return Response(response, status.HTTP_200_OK)
+        except Exception as ex:
+            response = {
+                "status": "error",
+                "message": ex,
+            }
+            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def post(self, request, format=None):
+        try:
+            data = request.data
+            serializer = self.serializer(data=data, many=False)
+
+            if serializer.is_valid():
+                serializer.save()
+                response = {
+                    "status": "success",
+                    "data": {
+                        "user": serializer.data,
+                    },
+                }
+                return Response(response, status.HTTP_200_OK)
+
+            response = {
+                "status": "fail",
+                "data": {
+                    "title": "Could not register the user",
+                    "message": serializer.errors,
+                },
+            }
+            return Response(response, status.HTTP_400_BAD_REQUEST)
+
+        except Exception as ex:
+            response = {
+                "status": "error",
+                "message": ex,
+            }
+            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
