8ee154e1f8012c7aa95e6b31426bf092d515abd6
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-02-03 16:10:44 -0400

    Added pair and refresh token views

--- app/user/views.py
+++ app/user/views.py
@@ -1,15 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from user.serializers import PairTokenSerializer, RefreshTokenSerializer
+from user import serializers
 
 # Create your views here.
 
 
 class PairTokenView(TokenObtainPairView):
-    serializer_class = PairTokenSerializer
+    serializer_class = serializers.PairTokenSerializer
 
 
 class RefreshTokenView(TokenRefreshView):
-    serializer_class = RefreshTokenSerializer
+    serializer_class = serializers.RefreshTokenSerializer
