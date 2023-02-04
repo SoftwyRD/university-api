a63e5e5b3a7271360581ee16f92584f854363d26
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-02-03 21:00:43 -0400

    Added jsend response for AuthenticationFailed exception

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.serializers import (
     TokenObtainPairSerializer,
     RefreshToken,
@@ -41,25 +42,47 @@ class UserSerializer(serializers.ModelSerializer):
 
 class PairTokenSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        data = super().validate(attrs)
-        response = {
-            "status": "success",
-            "data": {
-                "tokens": data,
-            },
-        }
+        try:
+            data = super().validate(attrs)
+            response = {
+                "status": "success",
+                "data": {
+                    "tokens": data,
+                },
+            }
 
-        return response
+            return response
+        except AuthenticationFailed as ex:
+            response = {
+                "status": "fail",
+                "data": {
+                    "title": "No active account",
+                    "message": str(ex),
+                },
+            }
+
+            raise AuthenticationFailed(response)
 
 
 class RefreshTokenSerializer(RefreshToken):
     def validate(self, attrs):
-        data = super().validate(attrs)
-        response = {
-            "status": "success",
-            "data": {
-                "token": data,
-            },
-        }
+        try:
+            data = super().validate(attrs)
+            response = {
+                "status": "success",
+                "data": {
+                    "token": data,
+                },
+            }
 
-        return response
+            return response
+        except AuthenticationFailed as ex:
+            response = {
+                "status": "fail",
+                "data": {
+                    "title": ex.args[1],
+                    "message": ex.args[0],
+                },
+            }
+
+            raise AuthenticationFailed(response)
