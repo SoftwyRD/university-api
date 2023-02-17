c0b51d3e5f73de66640df0206171c5442b403913
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-02-17 14:49:57 -0400

    Add client address to CORS
    Closes #39

--- app/app/settings.py
+++ app/app/settings.py
@@ -180,8 +180,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000/",
+]
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "University-API",
